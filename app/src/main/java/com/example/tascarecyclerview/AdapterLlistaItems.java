9563597ccb37c7ece5c8353727b524782a7f18cc
Christian Perona <cperona@jaumebalmes.net>
2022-12-15 18:38:16 +0100

    Recycler Updated, ItemDecoration & Animation

--- app/src/main/java/com/example/tascarecyclerview/AdapterLlistaItems.java
+++ app/src/main/java/com/example/tascarecyclerview/AdapterLlistaItems.java
@@ -3,6 +3,8 @@ package com.example.tascarecyclerview;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,9 +12,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class AdapterLlistaItems extends RecyclerView.Adapter<AdapterLlistaItems.ViewHolderLlistaItems>{
+public class AdapterLlistaItems extends RecyclerView.Adapter<AdapterLlistaItems.ViewHolderLlistaItems> implements View.OnClickListener{
     ArrayList<String> llistaItems;
     ArrayList<String> llistaSubitems;
+    private View.OnClickListener listener;
 
     public AdapterLlistaItems(ArrayList<String> llistaItems, ArrayList<String> llistaSubitems) {
         this.llistaItems = llistaItems;
@@ -25,18 +28,41 @@ public class AdapterLlistaItems extends RecyclerView.Adapter<AdapterLlistaItems.
     @Override
     public AdapterLlistaItems.ViewHolderLlistaItems onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.llista_items, null, false);
+        view.setOnClickListener(this);
         return new ViewHolderLlistaItems(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull AdapterLlistaItems.ViewHolderLlistaItems holder, int position) {
         holder.assignarItem(llistaItems.get(position), llistaSubitems.get(position));
+        //Afegim animació quan apareixen els items
+        setAnimation(holder.itemView, position);
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        int lastPosition = -1;
+        if (position > lastPosition)
+        {
+            Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(), android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 
     //Recompte d'items
     @Override
     public int getItemCount() {
         return llistaItems.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+    @Override
+    public void onClick(View view) {
+        if (listener != null) {
+            listener.onClick(view);
+        }
     }
 
     public class ViewHolderLlistaItems extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/tascarecyclerview/MainActivity.java
+++ app/src/main/java/com/example/tascarecyclerview/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.tascarecyclerview;
 
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         recyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
         //Omplim la llista d'items
         llistaItems = new ArrayList<>();
@@ -33,6 +41,24 @@ public class MainActivity extends AppCompatActivity {
         //Li passem la llista d'items a l'adapter
         AdapterLlistaItems adapterLlistaItems = new AdapterLlistaItems(llistaItems, llistaSubitems);
         recyclerView.setAdapter(adapterLlistaItems);
+
+        //Fem que els items siguin cliquejables
+        adapterLlistaItems.setOnClickListener(new View.OnClickListener() {
+            //Lo que s'executarà quan es cliqui un item de la llista
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Selecció: " + llistaItems.get(recyclerView.getChildAdapterPosition(view)), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Afegim separadors amb ItemDecoration
+        DividerItemDecoration dividerItemDecoration1 = new DividerItemDecoration(this, GridLayoutManager.VERTICAL);
+        DividerItemDecoration dividerItemDecoration2 = new DividerItemDecoration(this, GridLayoutManager.HORIZONTAL);
+        recyclerView.addItemDecoration(dividerItemDecoration1);
+        recyclerView.addItemDecoration(dividerItemDecoration2);
+
+        //Afegim transicions
+
 
     }
 }
